0adad1c2d2a1f01341278529ef3f44753935ff10
Ben Johnson <bjohnson@binarylogic.com>
2019-01-11 15:50:20 -0500

    Cast strings to atoms

--- lib/ecto_atom.ex
+++ lib/ecto_atom.ex
@@ -1,5 +1,4 @@
 defmodule Ecto.Atom do
-
   @moduledoc """
 
   Custom Type to support `:atom`
@@ -17,11 +16,11 @@ defmodule Ecto.Atom do
   def type, do: :string
 
   def cast(value) when is_atom(value), do: {:ok, value}
+  def cast(value) when is_binary(value), do: {:ok, String.to_atom(value)}
   def cast(_), do: :error
 
   def load(value), do: {:ok, String.to_existing_atom(value)}
 
   def dump(value) when is_atom(value), do: {:ok, Atom.to_string(value)}
   def dump(_), do: :error
-
 end
