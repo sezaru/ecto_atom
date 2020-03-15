1c9e915f4862bc0d8f4cb6dd6c8277a3009f6d48
Eduardo Barreto Alexandre <eduardo.alexandre@siemens-healthineers.com>
2020-03-15 13:03:15 -0300

    Fully implement Ecto.Type behaviour

--- lib/ecto_atom.ex
+++ lib/ecto_atom.ex
@@ -13,14 +13,24 @@ defmodule Ecto.Atom do
 
   @behaviour Ecto.Type
 
+  @impl Ecto.Type
   def type, do: :string
 
+  @impl Ecto.Type
   def cast(value) when is_atom(value), do: {:ok, value}
   def cast(value) when is_binary(value), do: {:ok, String.to_atom(value)}
   def cast(_), do: :error
 
+  @impl Ecto.Type
   def load(value), do: {:ok, String.to_existing_atom(value)}
 
+  @impl Ecto.Type
   def dump(value) when is_atom(value), do: {:ok, Atom.to_string(value)}
   def dump(_), do: :error
+
+  @impl Ecto.Type
+  def embed_as(_), do: :self
+
+  @impl Ecto.Type
+  def equal?(lhs, rhs), do: lhs == rhs
 end
